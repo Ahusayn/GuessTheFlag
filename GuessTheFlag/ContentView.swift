bfe04da886e799edde46e45703d668be19939c0b
Abdul <ahusaynn@gmail.com>
2024-09-07 19:51:25 +0300

    GuessTheFlag Updated

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -2,22 +2,124 @@
 //  ContentView.swift
 //  GuessTheFlag
 //
-//  Created by mac on 05/09/2024.
+//  Created by hssn on 05/09/2024.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+   @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+   @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var score = 0
+    @State private var questionNumber = 0
+    @State private var showingFinalScore = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            RadialGradient( stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 700)
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                Text("Guess the Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundStyle(.white)
+                
+                VStack(spacing: 15) {
+                    VStack() {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                           Image(countries[number])
+                               .clipShape(.capsule)
+                               .shadow(radius: 5)
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical,20)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: \(score)")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+                
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
+        .alert(scoreTitle, isPresented: $showingScore) {
+               Button("Continue", action: askQuestion)
+            } message: {
+                if scoreTitle == "Correct" {
+                    Text("Your score is \(score)")
+                } else {
+                    Text("Wrong! That’s the flag of \(countries[correctAnswer])")
+                }
+                
+                
+            }
+        
+            .alert("Game Over", isPresented: $showingFinalScore) {
+                Button("Restart", action: resetGame)
+            } message: {
+                Text("Your final score is \(score) / 8")
+            }
+
+           
+        }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            score += 1
+        } else {
+            scoreTitle = "Wrong"
+        }
+        showingScore = true
+        questionNumber += 1
+        if questionNumber == 8 {
+            showingFinalScore = true
+        }
+    }
+  
+     func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func resetGame() {
+        score = 0
+        questionNumber = 0
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        showingFinalScore = false
     }
 }
+    
+    
+    
+
 
 #Preview {
     ContentView()
